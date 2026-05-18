00d43ffce73a2b82ad3ae2bbee3f16d5ff543fa3
Meisam Rivers <meisam-on-github@googlegroups.com>
2026-05-18 06:23:37 +0000

    feat(algebra): define `sub`

---
+++ algebra/sub.h
@@ -0,0 +1,16 @@
+#ifndef CPP_ALGEBRA_SUB_H
+#define CPP_ALGEBRA_SUB_H
+
+#include <concepts>
+#include <optional>
+
+namespace algebra {
+
+  template<typename Rep>
+  concept Sub = requires(Rep rep, std::optional<int> a, std::optional<int> b) {
+    {rep.sub(a, b)} -> std::same_as<std::optional<int>>;
+  };
+
+}  // namespace algebra
+
+#endif  // CPP_ALGEBRA_SUB_H
---
+++ algebra/sub_test.cc
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+#include "sub.h"
+
+namespace {
+
+struct ValueSemanticsSub {
+  auto sub(std::optional<int> a, std::optional<int> b) -> std::optional<int> {
+    if (!a.has_value() || !b.has_value()) {
+      return std::nullopt;
+    }
+    return a.value() - b.value();
+  }
+};
+
+struct PointerSub {
+  auto sub(int *a, int *b) -> std::optional<int> {
+    if (a == nullptr || b == nullptr) {
+      return std::nullopt;
+    }
+    return *a - *b;
+  }
+};
+
+}  // namespace
+
+TEST(SubTest, ValueSemanticsSub_should_be_valid) {
+  static_assert(algebra::Sub<ValueSemanticsSub>
+     && "ValueSemanticsSub should be a valid sub");
+}
+
+TEST(SubTest, PointerSub_should_be_invalid) {
+  static_assert(!algebra::Sub<PointerSub>
+     && "PointerSub should not be a valid sub");
+}
