392b1422f5306676df58c636e981b249709c49d8
Meisam Rivers <meisam-on-github@googlegroups.com>
2026-05-18 05:37:01 +0000

    feat(algebra): add literal numbers

---
+++ algebra/literal.h
@@ -0,0 +1,16 @@
+#ifndef CPP_ALGEBRA_LITERAL_H
+#define CPP_ALGEBRA_LITERAL_H
+
+#include <concepts>
+#include <optional>
+
+namespace algebra {
+
+  template<typename Rep>
+  concept Literal = requires(Rep a, int n) {
+    {a.literal(n)} -> std::same_as<std::optional<int>>;
+  };
+
+}
+
+#endif  // CPP_ALGEBRA_LITERAL_H
