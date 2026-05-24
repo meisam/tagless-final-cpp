0c1d4210a4f9ae84eebbad6100f73eebb0496509
Meisam Rivers <meisam-on-github@googlegroups.com>
2026-05-24 04:03:34 +0000

    feat(runtime): define `basic_interpreter`

---
+++ runtime/basic_interpreter.h
@@ -0,0 +1,18 @@
+#ifndef CPP_RUNTIME_BASIC_INTERPRETER_H
+#define CPP_RUNTIME_BASIC_INTERPRETER_H
+
+#include <optional>
+
+namespace runtime {
+
+struct BasicInterpreter {
+  auto literal(int n) -> std::optional<int>;
+  auto add(std::optional<int> a, std::optional<int> b) -> std::optional<int>;
+  auto mul(std::optional<int> a, std::optional<int> b) -> std::optional<int>;
+  auto sub(std::optional<int> a, std::optional<int> b) -> std::optional<int>;
+  auto div(std::optional<int> a, std::optional<int> b) -> std::optional<int>;
+};
+
+}  // namespace runtime
+
+#endif  // CPP_RUNTIME_BASIC_INTERPRETER_H
